db99a1e0b3f6ea89fefba1438bcce315d8a7a047
safna100 <khadeejasafna857@gmail.com>
2026-01-26 12:30:49 +0530

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,7 @@
+$(document).ready(function(){
+    $('.toggle_btn').click(function(){
+        $('.toggle_btn').toggleClass('close_toggle');
+        // $('header nav ul').toggleClass('toggled');
+    })
+
+})
